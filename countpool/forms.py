0acfdc9027c3df85a38630e30344c2b64baceac6
Neil <neil.tesaluna@gmail.com>
2021-03-04 12:32:11 +1100

    changing date input to datetime-local

--- countpool/forms.py
+++ countpool/forms.py
@@ -1,12 +1,12 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired, Length
-from wtforms.fields.html5 import DateField
+from wtforms.fields.html5 import DateTimeLocalField
 
 class NewTimer(FlaskForm):
 
     title = StringField('Timer', validators=[DataRequired(), Length(min=2, max=20)])
 
-    date = DateField('Date', validators=[DataRequired()])
+    date = DateTimeLocalField('Date', validators=[DataRequired()], format='%Y-%m-%dT%H:%M')
 
     submit = SubmitField('New Timer')
